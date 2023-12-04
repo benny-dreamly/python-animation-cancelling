083d2d23bccc03b0c402815925c65529d45ecae7
Benny Dreamly <bgaming0277@gmail.com>
2023-12-03 20:14:06 -0700

    adding windows support, have to test on windows pc

--- macro.py
+++ macro.py
@@ -11,12 +11,20 @@ os = sys.platform
 #    PyKeyboard.press_keys(['rightshift', 'delete', 'r'])
 if os == 'darwin':
     windows_open = pygetwindow.getAllTitles()
-
+    if 'Stardew Valley ' in windows_open:
+        while True:
+            event = keyboard.read_event()
+            if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
+                keyboard.send('right_shift+r+del')
+if os == ('win32:'):
+    windows_open = pygetwindow.getAllWindows()
+    if '' in windows_open:
+        while True:
+            event = keyboard.read_event()
+            if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
+                keyboard.send('right_shift+r+del')
 if 'Stardew Valley ' in windows_open:
-    while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
-            keyboard.send('right_shift+r+del')
+
 
 
 
