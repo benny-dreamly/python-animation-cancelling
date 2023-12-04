a54265d2bf17b0312fae84c8fd67af64feb3ea0e
Benny Dreamly <bgaming0277@gmail.com>
2023-12-03 20:14:45 -0700

    added more concise code

--- macro.py
+++ macro.py
@@ -1,14 +1,9 @@
 import sys
 import keyboard
 import pygetwindow
-import getkey
-import pymouse
-from pykeyboard import PyKeyboard
 
 os = sys.platform
 
-#if keyboard.is_pressed('space'):
-#    PyKeyboard.press_keys(['rightshift', 'delete', 'r'])
 if os == 'darwin':
     windows_open = pygetwindow.getAllTitles()
     if 'Stardew Valley ' in windows_open:
@@ -23,7 +18,7 @@ if os == ('win32:'):
             event = keyboard.read_event()
             if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
                 keyboard.send('right_shift+r+del')
-if 'Stardew Valley ' in windows_open:
+
 
 
 
