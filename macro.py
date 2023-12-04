0b73f01386aa7096de3cc6ef7c2b64c5d336bfd5
Benny Dreamly <bgaming0277@gmail.com>
2023-12-03 20:20:00 -0700

    ✨added windows support

--- macro.py
+++ macro.py
@@ -12,8 +12,8 @@ if os == 'darwin':
             if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
                 keyboard.send('right_shift+r+del')
 if os == ('win32:'):
-    windows_open = pygetwindow.getAllWindows()
-    if '' in windows_open:
+    windows_open = pygetwindow.getAllTitles()
+    if 'SMAPI ' or 'Stardew Valley' in windows_open:
         while True:
             event = keyboard.read_event()
             if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
